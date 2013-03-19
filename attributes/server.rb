483a08f533a8c2e9748272ede6df56d86144da56
Felipe Cecagno <fcecagno@gmail.com>
2013-03-19 15:13:03 -0300

    make it optional to monitor chef nodes - this is useful when you want to monitor unmanaged hosts only; add support for perfdata; use databag for commands; make it possible to insert custom variables on unmanaged hosts; use the nagios config sample as base for the config template (with all comments and stuff);

--- attributes/server.rb
+++ attributes/server.rb
@@ -84,6 +84,7 @@ default['nagios']['ldap_authoritative'] = nil
 
 default['nagios']['templates'] = Mash.new
 default['nagios']['interval_length'] = 1
+default['nagios']['process_perf_data'] = false
 
 # Provide all interval values in seconds
 default['nagios']['default_host']['check_interval']     = 15
@@ -91,12 +92,14 @@ default['nagios']['default_host']['retry_interval']     = 15
 default['nagios']['default_host']['max_check_attempts'] = 1
 default['nagios']['default_host']['notification_interval'] = 300
 default['nagios']['default_host']['flap_detection'] = true
+default['nagios']['default_host']['perfdata_command'] = "process_host_perfdata_for_nagiosgraph"
 
 default['nagios']['default_service']['check_interval']     = 60
 default['nagios']['default_service']['retry_interval']     = 15
 default['nagios']['default_service']['max_check_attempts'] = 3
 default['nagios']['default_service']['notification_interval'] = 1200
 default['nagios']['default_service']['flap_detection'] = true
+default['nagios']['default_service']['perfdata_command'] = "process_service_perfdata_for_nagiosgraph"
 
 default['nagios']['server']['web_server'] = :apache
 default['nagios']['server']['nginx_dispatch'] = :cgi
